754ae441f099bec461e3f5deb47b39fdf8f707b4
Thomas Seilund <tps@netmaster.dk>
2023-12-12 11:55:50 +0100

    Initial Commit

---
+++ ICB.app.iOS.02/ICB_app_iOS_02App.swift
@@ -0,0 +1,17 @@
+//
+//  ICB_app_iOS_02App.swift
+//  ICB.app.iOS.02
+//
+//  Created by Thomas Seilund on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ICB_app_iOS_02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
